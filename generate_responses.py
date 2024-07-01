838666da1222612eee03ad9ecad0a73923a8fe92
Vijay Daita <vdaita@gmail.com>
2024-07-01 14:09:27 -0500

    batched

--- generate_responses.py
+++ generate_responses.py
@@ -21,6 +21,23 @@ def main(hf_model_id: str, model_type: OutputEnum, output_folder: str):
     dataset = load_dataset("nuprl/CanItEdit", split="test")
     pipe = pipeline(model=hf_model_id, torch_dtype=torch.bfloat16, device_map="auto")
 
+    dataset_inputs = []
+    for row in tqdm(dataset):
+        file_path = os.path.join(output_folder, f"{row['id']}_direct.txt")
+        if os.path.exists(file_path):
+            continue
+
+        old_contents = f"<TOP/>\n{row['before']}"
+        formatted_input = f"# File:\n{old_contents}\n# Instruction:{row['instruction_descriptive']}"
+        if model_type == "whole":
+            formatted_input += "\nPlease completely rewrite the file, adding  the changes from instruction. Output using a Python code block (start with ```python and end with ```)."
+        
+        dataset_inputs.append(formatted_input)
+
+    outputs = pipe(dataset_inputs)
+    outputs = [output[0]["generated_text"] for output in outputs]
+    dataset["outputs"] = outputs
+    
     model_type = model_type.value
 
     if not(os.path.exists(output_folder)):
@@ -35,8 +52,9 @@ def main(hf_model_id: str, model_type: OutputEnum, output_folder: str):
         formatted_input = f"# File:\n{old_contents}\n# Instruction:{row['instruction_descriptive']}"
         if model_type == "whole":
             formatted_input += "\nPlease completely rewrite the file, adding  the changes from instruction. Output using a Python code block (start with ```python and end with ```)."
-        output = pipe(formatted_input, do_sample=True, max_new_tokens=500, top_p=0.95, **{"use_cache": True})
-        output = output[0]["generated_text"]
+        # output = pipe(formatted_input, do_sample=True, max_new_tokens=500, top_p=0.95, **{"use_cache": True})
+        output = row['outputs']
+        output = output.replace(formatted_input, "")
         out_file = open(file_path, "w+")
         out_file.write(output)
         out_file.close()
