5289c9c691a5f3ec35f2d34a069e09ab1fb85e21
Vijay Daita <vdaita@gmail.com>
2024-07-01 23:52:52 -0500

    Updated to use prompt used in training

--- generate_responses.py
+++ generate_responses.py
@@ -11,6 +11,40 @@ import diff_utils
 import subprocess
 from enum import Enum
 
+shot = """## File:
+<TOP/>
+def multiply(a, b):
+    return a * b
+
+def add(a, b):
+    sum = a + b
+    return sum
+
+## Instruction:
+1. Remove the multiply function
+2. Make the add function more concise by replacing it with only a return statement
+3. Add a subtract function
+## Your output:
+<Delete>
+def multiply(a, b):
+    return a * b
+</Delete>
+<Replace>
+    sum = a + b
+    return sum
+<With>
+    return a + b
+</Replace>
+<Insert>
+def subtract(a, b):
+    return a - b
+<After>
+    sum = a + b
+    return sum
+
+</Insert>
+    """
+
 class OutputEnum(str, Enum):
     line = "line"
     ir = "ir"
@@ -27,10 +61,8 @@ def main(hf_model_id: str, model_type: OutputEnum, output_folder: str):
         if os.path.exists(file_path):
             continue
 
-        old_contents = f"<TOP/>\n{row['before']}"
-        formatted_input = f"# File:\n{old_contents}\n# Instruction:{row['instruction_descriptive']}"
-        if model_type == "whole":
-            formatted_input += "\nPlease completely rewrite the file, adding  the changes from instruction. Output using a Python code block (start with ```python and end with ```)."
+        # if model_type == "whole":
+        #     formatted_input += "\nPlease completely rewrite the file, adding  the changes from instruction. Output using a Python code block (start with ```python and end with ```)."
         
         dataset_inputs.append(formatted_input)
 
@@ -52,6 +84,19 @@ def main(hf_model_id: str, model_type: OutputEnum, output_folder: str):
         formatted_input = f"# File:\n{old_contents}\n# Instruction:{row['instruction_descriptive']}"
         if model_type == "whole":
             formatted_input += "\nPlease completely rewrite the file, adding  the changes from instruction. Output using a Python code block (start with ```python and end with ```)."
+        elif model_type == "ir":
+            formatted_input = f"""Generate insert-after, delete, and replace blocks to edit the given file according to the user's instruction. Here's an example:
+    {shot}
+
+    ## File:
+    <TOP/>
+    {row['before']}
+
+    ## Instruction: 
+    {row['instruction_descriptive']}
+
+    ## Your output:
+    """
         # output = pipe(formatted_input, do_sample=True, max_new_tokens=500, top_p=0.95, **{"use_cache": True})
         output = row['outputs']
         output = output.replace(formatted_input, "")
